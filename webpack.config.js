31eba80dcb1f20e5388b89a0849f04ec1a0fe349
naomigrace <naomigracep@gmail.com>
2018-05-11 13:22:13 -0400

    up to step 6

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require("path");
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
+
+module.exports = {
+  context: __dirname,
+
+  entry: './mysite/polls/static/js/index',
+
+  output: {
+      path: path.resolve('./mysite/polls/static/bundles/'),
+      filename: "[name]-[hash].js",
+  },
+
+  plugins: [
+    new BundleTracker({filename: './djangoproject/webpack-stats.json'}),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  }
+
+};
